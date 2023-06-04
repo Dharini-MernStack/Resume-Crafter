8e697a332cb3b1f0e6e6cdbbd23cf7ad25e23769
Dharini-MernStack <dharinisrinivasan.94@gmail.com>
2023-06-04 22:26:11 +0530

    Second change

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ResumeLogo from '../Assets/logo.jpeg';
+
 
 const Home = () => {
   return (
@@ -8,6 +10,7 @@ const Home = () => {
       <h1>Welcome to the Resume Builder aka Resume crafter</h1>
       <p><strong>Create your professional resume with ease</strong></p>
       <div>
+      <img src={ResumeLogo} alt="Resume Logo" className="resume-logo" />
         <Link to="/resume-form" className='link-button'>Start Building</Link>
       </div>
     </div>
